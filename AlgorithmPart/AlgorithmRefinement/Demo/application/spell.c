4a03256a02ac77295f25869fd8be66da94bd946a
sux <sux@ezogal.cn>
2021-03-02 16:20:49 +0800

    调整源文件结构

--- AlgorithmPart/AlgorithmRefinement/Demo/application/spell.c
+++ AlgorithmPart/AlgorithmRefinement/Demo/application/spell.c
@@ -2,7 +2,7 @@
 // Created by speauty on 2021/3/2.
 //
 #include <string.h>
-#include "search.h"
+#include "../search/search.h"
 #include "spell.h"
 
 static int compare_str(const void *key1, const void *key2)
